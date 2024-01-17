2913ac67bc84c5495ec90d05a37cc970cfedf6e8
PeliAzuh <47522008+krl299@users.noreply.github.com>
2024-01-17 23:57:13 +0100

    CreateDefaultFolders script

---
+++ CreateFolders.cs
@@ -0,0 +1,72 @@
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class CreateFolders : EditorWindow {
+
+    private static string projectName = "ProjectName";
+
+    /// <summary>
+    /// Creates an instance of the CreateFolders window, sets its position and size, and shows it as a popup.
+    /// </summary>
+    [MenuItem("Assets/Create Default Folders")]
+    private static void SetUpFolders(){
+        CreateFolders window = GetWindow<CreateFolders>();
+        window.position = new Rect(Screen.width / 2, Screen.height / 2, 400, 150);
+        window.ShowPopup();
+    }
+
+    /// <summary>
+    /// Creates a set of predefined folders in the Unity project's Assets directory.
+    /// </summary>
+    private static void CreateProjectFolders(){
+        List<string> folders = new List<string>{
+            "Animations",
+            "Audio",
+            "Editor",
+            "Materials",
+            "Models",
+            "Prefabs",
+            "Scripts",
+            "Scenes",
+            "Shaders",
+            "Textures",
+            "UI"
+        };
+
+        foreach(string folder in folders){
+            if (!Directory.Exists("Assets/" + folder)){
+                Directory.CreateDirectory("Assets/" + folder);
+            }
+        }
+
+        List<string> uiFolders = new List<string>{
+            "Assets",
+            "Fonts",
+            "Icons"
+        };
+
+        foreach(string folder in uiFolders){
+            if (!Directory.Exists("Assets/UI/" + folder)){
+                Directory.CreateDirectory("Assets/" + "UI/" + folder);
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+
+    /// <summary>
+    /// Handles the GUI events for the CreateFolders window.
+    /// </summary>
+    void OnGUI(){
+        EditorGUILayout.LabelField("Insert the Project name used as the root folder");
+        projectName = EditorGUILayout.TextField("Project Name", projectName);
+        this.Repaint();
+        GUILayout.Space(70);
+        if (GUILayout.Button("Create Folders")){
+            CreateProjectFolders();
+            this.Close();
+        }
+    }
+}
